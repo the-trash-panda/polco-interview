d94b75ffd289a8159167783bb7785e46d66787c2
Catherine Chiu <chiuycatherine@gmail.com>
2022-03-12 14:09:18 -0800

    Set up React app with Babel and Webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }
+    ]
+  }
+};
