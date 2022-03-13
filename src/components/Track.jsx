48cdd05e0325f7101dcf2fead758b9e99c48e55e
Catherine Chiu <chiuycatherine@gmail.com>
2022-03-12 16:39:46 -0800

    Add FontAwesomeIcon for favorite tracks

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faHeart)
 
 const Track = ({ track, setPage, setCurrentTrack }) => {
+
   return (
     <li
       onClick={(e) => {
@@ -9,7 +15,9 @@ const Track = ({ track, setPage, setCurrentTrack }) => {
       }
       }
     >
+      <FontAwesomeIcon icon="fas fa-heart" color="black"/>
       {track.name} by {track.artist.name}
+
     </li>
   )
 }
